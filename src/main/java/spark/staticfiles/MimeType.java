789ba83cd057ec02dbb09fd56d0674a17c1fdd84
A.Lepe <lepe@support.ne.jp>
2022-07-29 12:25:59 +0900

    Simple solution to #997 (Added multiple mime types and exposed mappings in a safe way)

--- src/main/java/spark/staticfiles/MimeType.java
+++ src/main/java/spark/staticfiles/MimeType.java
@@ -16,9 +16,9 @@
  */
 package spark.staticfiles;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 import spark.resource.AbstractFileResolvingResource;
 
@@ -31,38 +31,65 @@ public class MimeType {
 
     private static volatile boolean guessingOn = true;
 
-    private static Map<String, String> mappings = new HashMap();
+    public static final Map<String, String> mappings = new ConcurrentHashMap<>();
 
     static {
+        mappings.put("7z", "application/x-7z-compressed");
+        mappings.put("aac", "audio/aac");
+        mappings.put("abw", "application/x-abiword");
+        mappings.put("arc", "application/x-freearc");
         mappings.put("au", "audio/basic");
         mappings.put("avi", "video/msvideo,video/avi,video/x-msvideo");
+        mappings.put("azw", "application/vnd.amazon.ebook");
+        mappings.put("bin", "application/octet-stream");
         mappings.put("bmp", "image/bmp");
+        mappings.put("bz", "application/x-bzip");
         mappings.put("bz2", "application/x-bzip2");
+        mappings.put("cda", "application/x-cdf");
+        mappings.put("csh", "application/x-csh");
         mappings.put("css", "text/css");
-        mappings.put("dtd", "application/xml-dtd");
+        mappings.put("csv", "text/csv");
         mappings.put("doc", "application/msword");
         mappings.put("docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
         mappings.put("dotx", "application/vnd.openxmlformats-officedocument.wordprocessingml.template");
+        mappings.put("dtd", "application/xml-dtd");
         mappings.put("eot", "application/vnd.ms-fontobject");
+        mappings.put("epub", "application/epub+zip");
         mappings.put("es", "application/ecmascript");
         mappings.put("exe", "application/octet-stream");
         mappings.put("gif", "image/gif");
         mappings.put("gz", "application/x-gzip");
-        mappings.put("ico", "image/x-icon");
         mappings.put("hqx", "application/mac-binhex40");
         mappings.put("htm", "text/html");
         mappings.put("html", "text/html");
+        mappings.put("ico", "image/x-icon");
+        mappings.put("ics", "text/calendar");
         mappings.put("jar", "application/java-archive");
+        mappings.put("jpeg", "image/jpeg");
         mappings.put("jpg", "image/jpeg");
         mappings.put("js", "application/javascript");
-        mappings.put("mjs", "application/javascript");
         mappings.put("json", "application/json");
+        mappings.put("jsonld", "application/ld+json");
+        mappings.put("mid", "audio/midi");
         mappings.put("midi", "audio/x-midi");
+        mappings.put("mjpeg", "video/x-motion-jpeg");
+        mappings.put("mjs", "application/javascript");
         mappings.put("mp3", "audio/mpeg");
+        mappings.put("mp4", "video/mp4");
         mappings.put("mpeg", "video/mpeg");
+        mappings.put("mpkg", "application/vnd.apple.installer+xml");
+        mappings.put("ndjson", "application/x-ndjson");
+        mappings.put("odp", "application/vnd.oasis.opendocument.presentation");
+        mappings.put("ods", "application/vnd.oasis.opendocument.spreadsheet");
+        mappings.put("odt", "application/vnd.oasis.opendocument.text");
+        mappings.put("oga", "audio/ogg");
         mappings.put("ogg", "audio/vorbis,application/ogg");
+        mappings.put("ogv", "video/ogg");
+        mappings.put("ogx", "application/ogg");
+        mappings.put("opus", "audio/opus");
         mappings.put("otf", "application/font-otf");
         mappings.put("pdf", "application/pdf");
+        mappings.put("php", "application/x-httpd-php");
         mappings.put("pl", "application/x-perl");
         mappings.put("png", "image/png");
         mappings.put("potx", "application/vnd.openxmlformats-officedocument.presentationml.template");
@@ -72,29 +99,40 @@ public class MimeType {
         mappings.put("ps", "application/postscript");
         mappings.put("qt", "video/quicktime");
         mappings.put("ra", "audio/x-pn-realaudio,audio/vnd.rn-realaudio");
-        mappings.put("rar", "application/x-rar-compressed");
         mappings.put("ram", "audio/x-pn-realaudio,audio/vnd.rn-realaudio");
+        mappings.put("rar", "application/x-rar-compressed");
         mappings.put("rdf", "application/rdf,application/rdf+xml");
         mappings.put("rtf", "application/rtf");
         mappings.put("sgml", "text/sgml");
+        mappings.put("sh", "application/x-sh");
         mappings.put("sit", "application/x-stuffit");
         mappings.put("sldx", "application/vnd.openxmlformats-officedocument.presentationml.slide");
         mappings.put("svg", "image/svg+xml");
         mappings.put("swf", "application/x-shockwave-flash");
+        mappings.put("tar", "application/x-tar");
         mappings.put("tgz", "application/x-tar");
+        mappings.put("tif", "image/tiff");
         mappings.put("tiff", "image/tiff");
+        mappings.put("ts", "video/mp2t");
         mappings.put("tsv", "text/tab-separated-values");
         mappings.put("ttf", "application/font-ttf");
         mappings.put("txt", "text/plain");
+        mappings.put("vsd", "application/vnd.visio");
         mappings.put("wav", "audio/wav,audio/x-wav");
+        mappings.put("weba", "audio/webm");
+        mappings.put("webm", "video/webm");
+        mappings.put("webp", "image/webp");
         mappings.put("woff", "application/font-woff");
         mappings.put("woff2", "application/font-woff2");
+        mappings.put("xhtml", "application/xhtml+xml");
         mappings.put("xlam", "application/vnd.ms-excel.addin.macroEnabled.12");
         mappings.put("xls", "application/vnd.ms-excel");
         mappings.put("xlsb", "application/vnd.ms-excel.sheet.binary.macroEnabled.12");
         mappings.put("xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
         mappings.put("xltx", "application/vnd.openxmlformats-officedocument.spreadsheetml.template");
         mappings.put("xml", "application/xml");
+        mappings.put("xul", "application/vnd.mozilla.xul+xml");
+        mappings.put("yaml", "text/yaml");
         mappings.put("zip", "application/zip,application/x-compressed-zip");
     }
 
